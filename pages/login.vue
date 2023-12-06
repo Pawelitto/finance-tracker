5ecd4b41ab39ab8dd8c206b6b6fe188f6cd08d2a
Paweł Pietras <pawel.pietras144@gmail.com>
2023-12-06 19:53:15 +0100

    Authentication: Sing-In Form and Flow

---
+++ pages/login.vue
@@ -0,0 +1,39 @@
+<template>
+  <Ucard v-if="!success">
+    <template #header> Sign-in to Finance Tracker </template>
+
+    <form>
+      <UFormGroup
+        label="Email"
+        name="email"
+        class="mb-4"
+        :required="true"
+        help="You will recive an email with the confirmation link"
+      >
+        <UInput type="email" placeholder="Email" required />
+      </UFormGroup>
+
+      <UButton
+        type="submit"
+        variant="solid"
+        color="black"
+        @click="success = true"
+        >Sign-in</UButton
+      >
+    </form>
+  </Ucard>
+  <UCard v-else>
+    <template #header> Email has been sent </template>
+
+    <div class="text-center">
+      <p class="mb-4">
+        We have sent an email to <strong>rmsfil@gmail.com</strong> with a link
+        to sign-in
+      </p>
+      <p><strong>Important:</strong> The link will expire in 5 minutes.</p>
+    </div>
+  </UCard>
+</template>
+<script setup>
+const success = ref(false);
+</script>
